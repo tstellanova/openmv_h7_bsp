5575a1c9315f7cb3df25fb73f2e76e6f24568c81
Todd Stellanova <tstellanova@users.noreply.github.com>
2020-08-04 17:56:37 -0700

    fix build problem

--- examples/play.rs
+++ examples/play.rs
@@ -10,7 +10,7 @@ LICENSE: BSD3 (see LICENSE file)
 use panic_rtt_core::{self, rprintln, rtt_init_print};
 use cortex_m_rt::{entry, exception, ExceptionFrame};
 
-// use openmv_h7_bsp::peripherals;
+use openmv_h7_bsp::peripherals;
 
 use ehal::blocking::delay::DelayMs;
 use ehal::digital::v2::OutputPin;
@@ -18,18 +18,31 @@ use ehal::digital::v2::ToggleableOutputPin;
 use embedded_hal as ehal;
 
 
-// // cortex-m-rt calls this for serious faults.  can set a breakpoint to debug
-#[exception]
-fn HardFault(ef: &ExceptionFrame) -> ! {
-    panic!("{:#?}", ef);
-}
 
 #[entry]
 fn main() -> ! {
     rtt_init_print!(NoBlockTrim);
     rprintln!("--> MAIN --");
 
-    loop {
+    let     (
+        mut rgb_leds,
+        _ir_led,
+        mut delay_source,
+        _dcmi_data_pins,
 
+    )
+        = peripherals::setup();
+
+    loop {
+        for _ in 0..10 {
+            for _ in 0..10 {
+                let _ = rgb_leds.0.toggle();
+                delay_source.delay_ms(125u32);
+            }
+            let _ = rgb_leds.1.toggle();
+            delay_source.delay_ms(250u32);
+        }
+        let _ = rgb_leds.2.toggle();
+        delay_source.delay_ms(500u32);
     }
 }
